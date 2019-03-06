5e4bf41cd3226d9fbc62847a88c80d836f74c7f5
Eduardo Marossi <eduardom44@gmail.com>
2019-03-05 21:27:24 -0300

    exemplo de fontes para LCD

---
+++ Screens/LCD-Fonts/SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -0,0 +1,57 @@
+/*
+ * main.c
+ *
+ * Created: 05/03/2019 18:00:58
+ *  Author: eduardo
+ */ 
+
+#include <asf.h>
+#include "tfont.h"
+#include "sourcecodepro_28.h"
+#include "calibri_36.h"
+#include "arial_72.h"
+
+
+struct ili9488_opt_t g_ili9488_display_opt;
+
+void configure_lcd(void){
+	/* Initialize display parameter */
+	g_ili9488_display_opt.ul_width = ILI9488_LCD_WIDTH;
+	g_ili9488_display_opt.ul_height = ILI9488_LCD_HEIGHT;
+	g_ili9488_display_opt.foreground_color = COLOR_CONVERT(COLOR_WHITE);
+	g_ili9488_display_opt.background_color = COLOR_CONVERT(COLOR_WHITE);
+
+	/* Initialize LCD */
+	ili9488_init(&g_ili9488_display_opt);
+	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
+	
+}
+
+
+void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
+	char *p = text;
+	while(*p != NULL) {
+		char letter = *p;
+		int letter_offset = letter - font->start_char;
+		if(letter <= font->end_char) {
+			tChar *current_char = font->chars + letter_offset;
+			ili9488_draw_pixmap(x, y, current_char->image->width, current_char->image->height, current_char->image->data);
+			x += current_char->image->width + spacing;
+		}
+		p++;
+	}	
+}
+
+
+int main(void) {
+	board_init();
+	sysclk_init();	
+	configure_lcd();
+	
+	font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
+	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
+	font_draw_text(&arial_72, "102456", 50, 200, 2);
+	while(1) {
+		
+	}
+}
