2291635234586224fc6718d920280e61e125462f
Rafael Corsi <rafael.corsi@insper.edu.br>
2020-04-14 09:35:59 -0300

    add tc para OLED

---
+++ Screens/OLED-Xplained-Pro-SPI/OLED-Xplained-Pro-SPI/src/main.c
@@ -0,0 +1,24 @@
+#include <asf.h>
+
+#include "gfx_mono_ug_2832hsweg04.h"
+#include "gfx_mono_text.h"
+#include "sysfont.h"
+
+int main (void)
+{
+	board_init();
+	sysclk_init();
+	delay_init();
+
+  // Init OLED
+	gfx_mono_ssd1306_init();
+  
+  // Escreve na tela um circulo e um texto
+	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
+  gfx_mono_draw_string("mundo", 50,16, &sysfont);
+
+  /* Insert application code here, after the board has been initialized. */
+	while(1) {
+
+	}
+}
