9aa842397c916f442b40105eb3b69cf4db4f2514
Rafael Corsi <corsiferrao@gmail.com>
2021-03-22 13:25:31 -0300

    firmware demo hc-05

---
+++ Demos/HC05-Controle-Exemplo/firmware/src/main.c
@@ -0,0 +1,263 @@
+/************************************************************************
+* 5 semestre - Eng. da Computao - Insper
+*
+* 2021 - Exemplo com HC05 com RTOS
+*
+*/
+
+#include <asf.h>
+#include "conf_board.h"
+#include <string.h>
+
+/************************************************************************/
+/* defines                                                              */
+/************************************************************************/
+
+// LEDs
+#define LED_PIO      PIOC
+#define LED_PIO_ID   ID_PIOC
+#define LED_IDX      8
+#define LED_IDX_MASK (1 << LED_IDX)
+
+// Botão
+#define BUT_PIO      PIOA
+#define BUT_PIO_ID   ID_PIOA
+#define BUT_IDX      11
+#define BUT_IDX_MASK (1 << BUT_IDX)
+
+// usart (bluetooth ou serial)
+// Descomente para enviar dados
+// pela serial debug
+
+// #define DEBUG_SERIAL
+
+#ifdef DEBUG_SERIAL
+   #define UART_COMM USART1
+#else
+   #define UART_COMM USART0
+#endif
+
+/************************************************************************/
+/* RTOS                                                                 */
+/************************************************************************/
+
+#define TASK_BLUETOOTH_STACK_SIZE            (4096/sizeof(portSTACK_TYPE))
+#define TASK_BLUETOOTH_STACK_PRIORITY        (tskIDLE_PRIORITY)
+
+/************************************************************************/
+/* prototypes                                                           */
+/************************************************************************/
+
+extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
+		signed char *pcTaskName);
+extern void vApplicationIdleHook(void);
+extern void vApplicationTickHook(void);
+extern void vApplicationMallocFailedHook(void);
+extern void xPortSysTickHandler(void);
+
+/************************************************************************/
+/* constants                                                            */
+/************************************************************************/
+
+/************************************************************************/
+/* variaveis globais                                                    */
+/************************************************************************/
+
+/************************************************************************/
+/* RTOS application HOOK                                                */
+/************************************************************************/
+
+/* Called if stack overflow during execution */
+extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
+		signed char *pcTaskName) {
+	printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
+	/* If the parameters have been corrupted then inspect pxCurrentTCB to
+	 * identify which task has overflowed its stack.
+	 */
+	for (;;) {
+	}
+}
+
+/* This function is called by FreeRTOS idle task */
+extern void vApplicationIdleHook(void) {
+	pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+}
+
+/* This function is called by FreeRTOS each tick */
+extern void vApplicationTickHook(void) { }
+
+extern void vApplicationMallocFailedHook(void) {
+	/* Called if a call to pvPortMalloc() fails because there is insufficient
+	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+	internally by FreeRTOS API functions that create tasks, queues, software
+	timers, and semaphores.  The size of the FreeRTOS heap is set by the
+	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+
+	/* Force an assert. */
+	configASSERT( ( volatile void * ) NULL );
+}
+
+/************************************************************************/
+/* handlers / callbacks                                                 */
+/************************************************************************/
+
+/************************************************************************/
+/* funcoes                                                              */
+/************************************************************************/
+
+void io_init(void) {
+
+  // Ativa PIOs
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(BUT_PIO_ID);
+
+  // Configura Pinos
+	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT | PIO_DEBOUNCE);
+	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
+}
+
+static void configure_console(void) {
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate = CONF_UART_BAUDRATE,
+#if (defined CONF_UART_CHAR_LENGTH)
+		.charlength = CONF_UART_CHAR_LENGTH,
+#endif
+		.paritytype = CONF_UART_PARITY,
+#if (defined CONF_UART_STOP_BITS)
+		.stopbits = CONF_UART_STOP_BITS,
+#endif
+	};
+
+	/* Configure console UART. */
+	stdio_serial_init(CONF_UART, &uart_serial_options);
+
+	/* Specify that stdout should not be buffered. */
+#if defined(__GNUC__)
+	setbuf(stdout, NULL);
+#else
+	/* Already the case in IAR's Normal DLIB default configuration: printf()
+	 * emits one character at a time.
+	 */
+#endif
+}
+
+uint32_t usart_puts(uint8_t *pstring) {
+	uint32_t i ;
+
+	while(*(pstring + i))
+		if(uart_is_tx_empty(USART_COM))
+			usart_serial_putchar(USART_COM, *(pstring+i++));
+}
+
+void usart_put_string(Usart *usart, char str[]) {
+  usart_serial_write_packet(usart, str, strlen(str));
+}
+
+int usart_get_string(Usart *usart, char buffer[], int bufferlen, uint timeout_ms) {
+  uint timecounter = timeout_ms;
+  uint32_t rx;
+  uint32_t counter = 0;
+
+  while( (timecounter > 0) && (counter < bufferlen - 1)) {
+    if(usart_read(usart, &rx) == 0) {
+      buffer[counter++] = rx;
+    }
+    else{
+      timecounter--;
+      vTaskDelay(1);
+    }
+  }
+  buffer[counter] = 0x00;
+  return counter;
+}
+
+void usart_send_command(Usart *usart, char buffer_rx[], int bufferlen,
+                        char buffer_tx[], int timeout) {
+  usart_put_string(usart, buffer_tx);
+  usart_get_string(usart, buffer_rx, bufferlen, timeout);
+}
+
+void config_usart0(void) {
+  sysclk_enable_peripheral_clock(USART_COM_ID);
+  usart_serial_options_t config;
+  config.baudrate = 9600;
+  config.charlength = US_MR_CHRL_8_BIT;
+  config.paritytype = US_MR_PAR_NO;
+  config.stopbits = false;
+  usart_serial_init(USART_COM, &config);
+  usart_enable_tx(USART_COM);
+  usart_enable_rx(USART_COM);
+
+  // RX - PB0  TX - PB1
+  pio_configure(PIOB, PIO_PERIPH_C, (1 << 0), PIO_DEFAULT);
+  pio_configure(PIOB, PIO_PERIPH_C, (1 << 1), PIO_DEFAULT);
+}
+
+int hc05_init(void) {
+  char buffer_rx[128];
+  usart_send_command(USART0, buffer_rx, 1000, "AT", 100);
+  usart_send_command(USART0, buffer_rx, 1000, "AT", 100);
+  usart_send_command(USART0, buffer_rx, 1000, "AT+NAMEServer", 100);
+  usart_send_command(USART0, buffer_rx, 1000, "AT", 100);
+  usart_send_command(USART0, buffer_rx, 1000, "AT+PIN0000", 100);
+}
+
+/************************************************************************/
+/* TASKS                                                                */
+/************************************************************************/
+
+void task_bluetooth(void) {
+
+  printf("Task Bluetooth started \n");
+
+#ifndef DEBUG_SERIAL
+  printf("Inicializando HC05 \n");
+  config_usart0();
+  hc05_init();
+#endif
+
+  // configura LEDs e Botões
+  io_init();
+
+  char button1 = '0';
+  char eof = 'X';
+
+  // Task não deve retornar.
+  while(1){
+		if(pio_get(LED_PIO, PIO_INPUT, LED_IDX_MASK) == 0) {
+      button1 = '1';
+    } else {
+      button1 = '0';
+    }
+
+    while(!usart_is_tx_ready(UART_COMM));
+    usart_write(UART_COMM, button1);
+    while(!usart_is_tx_ready(UART_COMM));
+    usart_write(UART_COMM, eof);
+
+    vTaskDelay( 500 / portTICK_PERIOD_MS);
+  }
+}
+
+/************************************************************************/
+/* main                                                                 */
+/************************************************************************/
+
+int main(void) {
+	/* Initialize the SAM system */
+	sysclk_init();
+	board_init();
+
+	configure_console();
+
+	/* Create task to make led blink */
+	xTaskCreate(task_bluetooth, "BLT", TASK_BLUETOOTH_STACK_SIZE, NULL,	TASK_BLUETOOTH_STACK_PRIORITY, NULL);
+
+	/* Start the scheduler. */
+	vTaskStartScheduler();
+
+	while(1){}
+
+	/* Will only get here if there was insufficient memory to create the idle task. */
+	return 0;
+}
