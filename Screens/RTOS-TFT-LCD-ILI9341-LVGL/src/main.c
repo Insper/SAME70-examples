225fc7668559c0862b87956008f88955f740d50c
Rafael Corsi <rafael.corsi@insper.edu.br>
2021-10-21 15:01:22 -0300

    port to lvgl 8

--- Screens/RTOS-TFT-LCD-ILI9341-LVGL/src/main.c
+++ Screens/RTOS-TFT-LCD-ILI9341-LVGL/src/main.c
@@ -9,14 +9,19 @@
 #include "touch/touch.h"
 
 /************************************************************************/
-/* STATIC                                                               */
+/* LCD / LVGL                                                           */
 /************************************************************************/
 
+#define LV_HOR_RES_MAX          (320)
+#define LV_VER_RES_MAX          (240)
+
 /*A static or global variable to store the buffers*/
-static lv_disp_buf_t disp_buf;
+static lv_disp_draw_buf_t disp_buf;
 
 /*Static or global buffer(s). The second buffer is optional*/
 static lv_color_t buf_1[LV_HOR_RES_MAX * LV_VER_RES_MAX];
+static lv_disp_drv_t disp_drv;          /*A variable to hold the drivers. Must be static or global.*/
+static lv_indev_drv_t indev_drv;
 
 /************************************************************************/
 /* RTOS                                                                 */
@@ -32,8 +37,8 @@ extern void vApplicationMallocFailedHook(void);
 extern void xPortSysTickHandler(void);
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask, signed char *pcTaskName) {
-  printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
-  for (;;) {	}
+	printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
+	for (;;) {	}
 }
 
 extern void vApplicationIdleHook(void) { }
@@ -41,41 +46,44 @@ extern void vApplicationIdleHook(void) { }
 extern void vApplicationTickHook(void) { }
 
 extern void vApplicationMallocFailedHook(void) {
-  configASSERT( ( volatile void * ) NULL );
+	configASSERT( ( volatile void * ) NULL );
 }
 
 /************************************************************************/
 /* lvgl                                                                 */
 /************************************************************************/
 
-static void event_handler(lv_obj_t * obj, lv_event_t event) {
-  if(event == LV_EVENT_CLICKED) {
-    printf("Clicked\n");
-  }
-  else if(event == LV_EVENT_VALUE_CHANGED) {
-    printf("Toggled\n");
-  }
+static void event_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
+
+	if(code == LV_EVENT_CLICKED) {
+		LV_LOG_USER("Clicked");
+	}
+	else if(code == LV_EVENT_VALUE_CHANGED) {
+		LV_LOG_USER("Toggled");
+	}
 }
 
 void lv_ex_btn_1(void) {
-  lv_obj_t * label;
+	lv_obj_t * label;
 
-  lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_set_event_cb(btn1, event_handler);
-  lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, -40);
+	lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
 
-  label = lv_label_create(btn1, NULL);
-  lv_label_set_text(label, "Button");
+	label = lv_label_create(btn1);
+	lv_label_set_text(label, "Corsi");
+	lv_obj_center(label);
 
-  lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_set_event_cb(btn2, event_handler);
-  lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 40);
-  lv_btn_set_checkable(btn2, true);
-  lv_btn_toggle(btn2);
-  lv_btn_set_fit2(btn2, LV_FIT_NONE, LV_FIT_TIGHT);
+	lv_obj_t * btn2 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
+	lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+	lv_obj_set_height(btn2, LV_SIZE_CONTENT);
 
-  label = lv_label_create(btn2, NULL);
-  lv_label_set_text(label, "Toggled");
+	label = lv_label_create(btn2);
+	lv_label_set_text(label, "Toggle");
+	lv_obj_center(label);
 }
 
 /************************************************************************/
@@ -83,15 +91,15 @@ void lv_ex_btn_1(void) {
 /************************************************************************/
 
 static void task_lcd(void *pvParameters) {
-  int px, py;
+	int px, py;
 
-  lv_ex_btn_1();
+	lv_ex_btn_1();
 
-  for (;;)  {
-    lv_tick_inc(50);
-    lv_task_handler();
-    vTaskDelay(50);
-  }
+	for (;;)  {
+		lv_tick_inc(50);
+		lv_task_handler();
+		vTaskDelay(50);
+	}
 }
 
 /************************************************************************/
@@ -99,29 +107,31 @@ static void task_lcd(void *pvParameters) {
 /************************************************************************/
 
 static void configure_lcd(void) {
-  /**LCD pin configure on SPI*/
-  pio_configure_pin(LCD_SPI_MISO_PIO, LCD_SPI_MISO_FLAGS);  //
-  pio_configure_pin(LCD_SPI_MOSI_PIO, LCD_SPI_MOSI_FLAGS);
-  pio_configure_pin(LCD_SPI_SPCK_PIO, LCD_SPI_SPCK_FLAGS);
-  pio_configure_pin(LCD_SPI_NPCS_PIO, LCD_SPI_NPCS_FLAGS);
-  pio_configure_pin(LCD_SPI_RESET_PIO, LCD_SPI_RESET_FLAGS);
-  pio_configure_pin(LCD_SPI_CDS_PIO, LCD_SPI_CDS_FLAGS);
-  
+	/**LCD pin configure on SPI*/
+	pio_configure_pin(LCD_SPI_MISO_PIO, LCD_SPI_MISO_FLAGS);  //
+	pio_configure_pin(LCD_SPI_MOSI_PIO, LCD_SPI_MOSI_FLAGS);
+	pio_configure_pin(LCD_SPI_SPCK_PIO, LCD_SPI_SPCK_FLAGS);
+	pio_configure_pin(LCD_SPI_NPCS_PIO, LCD_SPI_NPCS_FLAGS);
+	pio_configure_pin(LCD_SPI_RESET_PIO, LCD_SPI_RESET_FLAGS);
+	pio_configure_pin(LCD_SPI_CDS_PIO, LCD_SPI_CDS_FLAGS);
+	
+	ili9341_init();
+	ili9341_backlight_on();
 }
 
 static void configure_console(void) {
-  const usart_serial_options_t uart_serial_options = {
-    .baudrate = USART_SERIAL_EXAMPLE_BAUDRATE,
-    .charlength = USART_SERIAL_CHAR_LENGTH,
-    .paritytype = USART_SERIAL_PARITY,
-    .stopbits = USART_SERIAL_STOP_BIT,
-  };
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate = USART_SERIAL_EXAMPLE_BAUDRATE,
+		.charlength = USART_SERIAL_CHAR_LENGTH,
+		.paritytype = USART_SERIAL_PARITY,
+		.stopbits = USART_SERIAL_STOP_BIT,
+	};
 
-  /* Configure console UART. */
-  stdio_serial_init(CONSOLE_UART, &uart_serial_options);
+	/* Configure console UART. */
+	stdio_serial_init(CONSOLE_UART, &uart_serial_options);
 
-  /* Specify that stdout should not be buffered. */
-  setbuf(stdout, NULL);
+	/* Specify that stdout should not be buffered. */
+	setbuf(stdout, NULL);
 }
 
 /************************************************************************/
@@ -129,70 +139,67 @@ static void configure_console(void) {
 /************************************************************************/
 
 void my_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p) {
-  ili9341_set_top_left_limit(area->x1, area->y1);   ili9341_set_bottom_right_limit(area->x2, area->y2);
-  ili9341_copy_pixels_to_screen(color_p,  (area->x2 - area->x1) * (area->y2 - area->y1));
-  
-  /* IMPORTANT!!!
-  * Inform the graphics library that you are ready with the flushing*/
-  lv_disp_flush_ready(disp_drv);
+	ili9341_set_top_left_limit(area->x1, area->y1);   ili9341_set_bottom_right_limit(area->x2, area->y2);
+	ili9341_copy_pixels_to_screen(color_p,  (area->x2 - area->x1) * (area->y2 - area->y1));
+	
+	/* IMPORTANT!!!
+	* Inform the graphics library that you are ready with the flushing*/
+	lv_disp_flush_ready(disp_drv);
 }
 
-bool my_input_read(lv_indev_drv_t * drv, lv_indev_data_t*data) {
-  int px, py, pressed;
-  
-  if (readPoint(&px, &py)) {
-    data->state = LV_INDEV_STATE_PR;
-  }
-  else {
-    data->state = LV_INDEV_STATE_REL;
-  }
-  
-  data->point.x = px;
-  data->point.y = py;
-  return false; /*No buffering now so no more data read*/
+void my_input_read(lv_indev_drv_t * drv, lv_indev_data_t*data) {
+	int px, py, pressed;
+	
+	if (readPoint(&px, &py))
+		data->state = LV_INDEV_STATE_PRESSED;
+	else
+		data->state = LV_INDEV_STATE_RELEASED; 
+	
+	data->point.x = px;
+	data->point.y = py;
+}
+
+void configure_lvgl(void) {
+	lv_init();
+	lv_disp_draw_buf_init(&disp_buf, buf_1, NULL, LV_HOR_RES_MAX * LV_VER_RES_MAX);
+	
+	lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
+	disp_drv.draw_buf = &disp_buf;          /*Set an initialized buffer*/
+	disp_drv.flush_cb = my_flush_cb;        /*Set a flush callback to draw to the display*/
+	disp_drv.hor_res = LV_HOR_RES_MAX;      /*Set the horizontal resolution in pixels*/
+	disp_drv.ver_res = LV_VER_RES_MAX;      /*Set the vertical resolution in pixels*/
+
+	lv_disp_t * disp;
+	disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
+	
+	/* Init input on LVGL */
+	lv_indev_drv_init(&indev_drv);
+	indev_drv.type = LV_INDEV_TYPE_POINTER;
+	indev_drv.read_cb = my_input_read;
+	lv_indev_t * my_indev = lv_indev_drv_register(&indev_drv);
 }
 
 /************************************************************************/
 /* main                                                                 */
 /************************************************************************/
 int main(void) {
-  /* board and sys init */
-  board_init();
-  sysclk_init();
-  configure_console();
+	/* board and sys init */
+	board_init();
+	sysclk_init();
+	configure_console();
 
-  /* LCd int */
-  configure_lcd();
-  ili9341_init();
-  configure_touch();
-  ili9341_backlight_on();
-  
-  /*LittlevGL init*/
-  lv_init();
-  lv_disp_drv_t disp_drv;                 /*A variable to hold the drivers. Can be local variable*/
-  lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
-  lv_disp_buf_init(&disp_buf, buf_1, NULL, LV_HOR_RES_MAX * LV_VER_RES_MAX);  /*Initialize `disp_buf` with the buffer(s) */
-  disp_drv.buffer = &disp_buf;            /*Set an initialized buffer*/
-  disp_drv.flush_cb = my_flush_cb;        /*Set a flush callback to draw to the display*/
-  lv_disp_t * disp;
-  disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
-  
-  /* Init input on LVGL */
-  lv_indev_drv_t indev_drv;
-  lv_indev_drv_init(&indev_drv);      /*Basic initialization*/
-  indev_drv.type = LV_INDEV_TYPE_POINTER;
-  indev_drv.read_cb = my_input_read;
-  /*Register the driver in LVGL and save the created input device object*/
-  lv_indev_t * my_indev = lv_indev_drv_register(&indev_drv);
-  
-  /* Create task to control oled */
-  if (xTaskCreate(task_lcd, "LCD", TASK_LCD_STACK_SIZE, NULL, TASK_LCD_STACK_PRIORITY, NULL) != pdPASS) {
-    printf("Failed to create lcd task\r\n");
-  }
-  
-  /* Start the scheduler. */
-  vTaskStartScheduler();
+	/* LCd, touch and lvgl init*/
+	configure_lcd();
+	configure_touch();
+	configure_lvgl();
 
-  /* RTOS n?o deve chegar aqui !! */
-  while(1){ }
+	/* Create task to control oled */
+	if (xTaskCreate(task_lcd, "LCD", TASK_LCD_STACK_SIZE, NULL, TASK_LCD_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create lcd task\r\n");
+	}
+	
+	/* Start the scheduler. */
+	vTaskStartScheduler();
+
+	while(1){ }
 }
