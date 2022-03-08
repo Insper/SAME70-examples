340971b51ea53b89116f6e31922ef88ff8d7fea3
Rafael Corsi <corsiferrao@gmail.com>
2022-03-08 18:01:16 -0300

    Update main.c

--- Perifericos-uC/TC-IRQ/TC-IRQ/src/main.c
+++ Perifericos-uC/TC-IRQ/TC-IRQ/src/main.c
@@ -12,86 +12,67 @@
 /* VAR globais                                                          */
 /************************************************************************/
 
-volatile char flag_tc = 0;
-
 /************************************************************************/
 /* PROTOTYPES                                                           */
 /************************************************************************/
 
 void LED_init(int estado);
 void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
+void pin_toggle(Pio *pio, uint32_t mask);
 
 /************************************************************************/
 /* Handlers                                                             */
 /************************************************************************/
 
 /**
-*  Handle Interrupcao botao 1
-*/
-static void Button1_Handler(uint32_t id, uint32_t mask)
-{
-}
-
-/**
 *  Interrupt handler for TC1 interrupt.
 */
-void TC1_Handler(void){
-	volatile uint32_t ul_dummy;
-
-	/****************************************************************
+void TC1_Handler(void) {
+	/**
 	* Devemos indicar ao TC que a interrupção foi satisfeita.
-	******************************************************************/
-	ul_dummy = tc_get_status(TC0, 1);
+	* Isso é realizado pela leitura do status do periférico
+	**/
+	volatile uint32_t status = tc_get_status(TC0, 1);
 
-	/* Avoid compiler warning */
-	UNUSED(ul_dummy);
-
-	/** Muda o estado do LED */
-	flag_tc = 1;
+	/** Muda o estado do LED (pisca) **/
+	pin_toggle(LED_PIO, LED_IDX_MASK);  
 }
 
 /************************************************************************/
 /* Funcoes                                                              */
 /************************************************************************/
 
-/*
- * @Brief Pisca LED placa
- */
-void pisca_led(int n, int t){
-  for (int i=0;i<n;i++){
-    pio_clear(LED_PIO, LED_IDX_MASK);
-    delay_ms(t);
-    pio_set(LED_PIO, LED_IDX_MASK);
-    delay_ms(t);
-  }
-}
-
-
 /**
 * @Brief Inicializa o pino do LED
 */
-void LED_init(int estado){
+void LED_init(int estado) {
 	pmc_enable_periph_clk(LED_PIO_ID);
 	pio_set_output(LED_PIO, LED_IDX_MASK, estado, 0, 0);
 };
 
+void pin_toggle(Pio *pio, uint32_t mask) {
+  if(pio_get_output_data_status(pio, mask))
+    pio_clear(pio, mask);
+  else
+    pio_set(pio,mask);
+}
 
 /**
 * Configura TimerCounter (TC) para gerar uma interrupcao no canal (ID_TC e TC_CHANNEL)
 * na taxa de especificada em freq.
-*/
+* O TimerCounter é meio confuso
+* o uC possui 3 TCs, cada TC possui 3 canais
+*	TC0 : ID_TC0, ID_TC1, ID_TC2
+*	TC1 : ID_TC3, ID_TC4, ID_TC5
+*	TC2 : ID_TC6, ID_TC7, ID_TC8
+*
+**/
 void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
 	uint32_t ul_div;
 	uint32_t ul_tcclks;
 	uint32_t ul_sysclk = sysclk_get_cpu_hz();
 
 	/* Configura o PMC */
-	/* O TimerCounter é meio confuso
-	o uC possui 3 TCs, cada TC possui 3 canais
-	TC0 : ID_TC0, ID_TC1, ID_TC2
-	TC1 : ID_TC3, ID_TC4, ID_TC5
-	TC2 : ID_TC6, ID_TC7, ID_TC8
-	*/
 	pmc_enable_periph_clk(ID_TC);
 
 	/** Configura o TC para operar em  freq hz e interrupçcão no RC compare */
@@ -99,14 +80,10 @@ void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
 	tc_init(TC, TC_CHANNEL, ul_tcclks | TC_CMR_CPCTRG);
 	tc_write_rc(TC, TC_CHANNEL, (ul_sysclk / ul_div) / freq);
 
-	/* Configura e ativa interrupçcão no TC canal 0 */
-	/* Interrupção no C */
-  NVIC_SetPriority(ID_TC, 4);
+	/* Configura NVIC*/
+  	NVIC_SetPriority(ID_TC, 4);
 	NVIC_EnableIRQ((IRQn_Type) ID_TC);
 	tc_enable_interrupt(TC, TC_CHANNEL, TC_IER_CPCS);
-
-	/* Inicializa o canal 0 do TC */
-	tc_start(TC, TC_CHANNEL);
 }
 
 /************************************************************************/
@@ -122,14 +99,14 @@ int main(void){
 	/* Configura Leds */
 	LED_init(0);
 
-  /** Configura timer TC0, canal 1 */
+	/**
+	 * Configura timer TC0, canal 1 
+	 * e inicializa contagem 
+	 */
 	TC_init(TC0, ID_TC1, 1, 2);
-  
+	tc_start(TC, TC_CHANNEL);
+
 	while (1) {
-            if(flag_tc){
-                pisca_led(1,10);
-                flag_tc = 0;
-            }
             pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 	}
 }
