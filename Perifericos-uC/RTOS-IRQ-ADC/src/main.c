9a3d4629e99f3f90bf0264e4913017d5e0514677
Rafael Corsi <corsiferrao@gmail.com>
2022-04-13 11:40:37 -0300

    lab rtos

--- Perifericos-uC/RTOS-IRQ-ADC/src/main.c
+++ Perifericos-uC/RTOS-IRQ-ADC/src/main.c
@@ -5,27 +5,19 @@
 /* BOARD CONFIG                                                         */
 /************************************************************************/
 
-#define BUT_PIO PIOA
-#define BUT_PIO_ID ID_PIOA
-#define BUT_PIO_PIN 11
-#define BUT_PIO_PIN_MASK (1 << BUT_PIO_PIN)
-
-#define LED_PIO PIOC
-#define LED_PIO_ID ID_PIOC
-#define LED_PIO_IDX 8
-#define LED_IDX_MASK (1 << LED_PIO_IDX)
-
 #define USART_COM_ID ID_USART1
 #define USART_COM USART1
+
+#define AFEC_POT AFEC1
+#define AFEC_POT_ID ID_AFEC1
+#define AFEC_POT_CHANNEL 6 // Canal do pino PC31
 
 /************************************************************************/
 /* RTOS                                                                */
 /************************************************************************/
 
-#define TASK_LED_STACK_SIZE (1024 / sizeof(portSTACK_TYPE))
-#define TASK_LED_STACK_PRIORITY (tskIDLE_PRIORITY)
-#define TASK_BUT_STACK_SIZE (2048 / sizeof(portSTACK_TYPE))
-#define TASK_BUT_STACK_PRIORITY (tskIDLE_PRIORITY)
+#define TASK_ADC_STACK_SIZE (1024 / sizeof(portSTACK_TYPE))
+#define TASK_ADC_STACK_PRIORITY (tskIDLE_PRIORITY)
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
                                           signed char *pcTaskName);
@@ -38,28 +30,27 @@ extern void xPortSysTickHandler(void);
 /* recursos RTOS                                                        */
 /************************************************************************/
 
-/** Semaforo a ser usado pela task led */
-SemaphoreHandle_t xSemaphoreBut;
-
 /** Queue for msg log send data */
-QueueHandle_t xQueueLedFreq;
+QueueHandle_t xQueueTouch;
+
+typedef struct {
+  uint value;
+} adcData;
 
 /************************************************************************/
 /* prototypes local                                                     */
 /************************************************************************/
 
-void but_callback(void);
-static void BUT_init(void);
-void pin_toggle(Pio *pio, uint32_t mask);
 static void USART1_init(void);
+TC_init(Tc *TC, int ID_TC, int TC_CHANNEL, int freq);
+config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
+                afec_callback_t callback);
+configure_console(void);
 
 /************************************************************************/
 /* RTOS application funcs                                               */
 /************************************************************************/
 
-/**
- * \brief Called if stack overflow during execution
- */
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
                                           signed char *pcTaskName) {
   printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
@@ -70,14 +61,8 @@ extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
   }
 }
 
-/**
- * \brief This function is called by FreeRTOS idle task
- */
 extern void vApplicationIdleHook(void) { pmc_sleep(SAM_PM_SMODE_SLEEP_WFI); }
 
-/**
- * \brief This function is called by FreeRTOS each tick
- */
 extern void vApplicationTickHook(void) {}
 
 extern void vApplicationMallocFailedHook(void) {
@@ -95,62 +80,44 @@ extern void vApplicationMallocFailedHook(void) {
 /* handlers / callbacks                                                 */
 /************************************************************************/
 
-void but_callback(void) {
-  BaseType_t xHigherPriorityTaskWoken = pdTRUE;
-  xSemaphoreGiveFromISR(xSemaphoreBut, &xHigherPriorityTaskWoken);
+void TC1_Handler(void) {
+  volatile uint32_t ul_dummy;
+
+  ul_dummy = tc_get_status(TC0, 1);
+
+  /* Avoid compiler warning */
+  UNUSED(ul_dummy);
+
+  /* Selecina canal e inicializa conversão */
+  afec_channel_enable(AFEC_POT, AFEC_POT_CHANNEL);
+  afec_start_software_conversion(AFEC_POT);
+}
+
+static void AFEC_pot_Callback(void) {
+  adcData adc;
+  adc.value = afec_channel_get_value(AFEC_POT, AFEC_POT_CHANNEL);
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  xQueueSendFromISR(xQueueADC, &adc, &xHigherPriorityTaskWoken);
 }
 
 /************************************************************************/
 /* TASKS                                                                */
 /************************************************************************/
 
-static void task_led(void *pvParameters) {
+static void task_adc(void *pvParameters) {
 
-  LED_init(1);
+  // configura ADC e TC para controlar a leitura
+  config_AFEC_pot(AFEC_POT, AFEC_POT_ID, AFEC_POT_CHANNEL, AFEC_pot_Callback);
+  TC_init(TC0, ID_TC1, 1, 60);
 
-  uint32_t msg = 0;
-  uint32_t delayTicks = 2000;
+  // variável para recever dados da fila
+  adcData adc;
 
-  /* tarefas de um RTOS não devem retornar */
-  for (;;) {
-    /* verifica se chegou algum dado na queue, e espera por 0 ticks */
-    if (xQueueReceive(xQueueLedFreq, &msg, (TickType_t)0)) {
-      /* chegou novo valor, atualiza delay ! */
-      /* aqui eu poderia verificar se msg faz sentido (se esta no range certo)
-       */
-      /* converte ms -> ticks */
-      delayMs = msg / portTICK_PERIOD_MS;
-      printf("delay = %d \n", delayMs);
-    }
-
-    /* pisca LED */
-    pin_toggle(LED_PIO, LED_IDX_MASK);
-
-    /* suspende por delayMs */
-    vTaskDelay(delayTicks / portTICK_PERIOD_MS);
-  }
-}
-
-static void task_but(void *pvParameters) {
-
-  /* iniciliza botao */
-  BUT_init();
-
-  uint32_t delayTicks = 2000;
-
-  for (;;) {
-    /* aguarda por tempo inderteminado até a liberacao do semaforo */
-    if (xSemaphoreTake(xSemaphoreBut, 0)) {
-      /* atualiza frequencia */
-      delayTicks -= 100;
-
-      /* envia nova frequencia para a task_led */
-      xQueueSend(xQueueLedFreq, (void *)&delayTicks, 10);
-
-      /* garante range da freq. */
-      if (delayTicks == 100) {
-        delayTicks = 900;
-      }
+  while (1) {
+    if (xQueueReceive(xQueueADC, &(adc), 1000)) {
+      printf("ADC: %d", adc);
+    } else {
+      printf("Nao chegou um novo dado em 1 segundo")
     }
   }
 }
@@ -177,29 +144,67 @@ static void configure_console(void) {
   setbuf(stdout, NULL);
 }
 
-/************************************************************************/
-/* inits                                                                */
-/************************************************************************/
+static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
+                            afec_callback_t callback) {
+  /*************************************
+   * Ativa e configura AFEC
+   *************************************/
+  /* Ativa AFEC - 0 */
+  afec_enable(afec);
 
-void pin_toggle(Pio *pio, uint32_t mask) {
-  if (pio_get_output_data_status(pio, mask))
-    pio_clear(pio, mask);
-  else
-    pio_set(pio, mask);
+  /* struct de configuracao do AFEC */
+  struct afec_config afec_cfg;
+
+  /* Carrega parametros padrao */
+  afec_get_config_defaults(&afec_cfg);
+
+  /* Configura AFEC */
+  afec_init(afec, &afec_cfg);
+
+  /* Configura trigger por software */
+  afec_set_trigger(afec, AFEC_TRIG_SW);
+
+  /*** Configuracao específica do canal AFEC ***/
+  struct afec_ch_config afec_ch_cfg;
+  afec_ch_get_config_defaults(&afec_ch_cfg);
+  afec_ch_cfg.gain = AFEC_GAINVALUE_0;
+  afec_ch_set_config(afec, afec_channel, &afec_ch_cfg);
+
+  /*
+  * Calibracao:
+  * Because the internal ADC offset is 0x200, it should cancel it and shift
+  down to 0.
+  */
+  afec_channel_set_analog_offset(afec, afec_channel, 0x200);
+
+  /***  Configura sensor de temperatura ***/
+  struct afec_temp_sensor_config afec_temp_sensor_cfg;
+
+  afec_temp_sensor_get_config_defaults(&afec_temp_sensor_cfg);
+  afec_temp_sensor_set_config(afec, &afec_temp_sensor_cfg);
+
+  /* configura IRQ */
+  afec_set_callback(afec, afec_channel, callback, 1);
+  NVIC_SetPriority(afec_id, 4);
+  NVIC_EnableIRQ(afec_id);
 }
 
-static void BUT_init(void) {
-  /* configura prioridae */
-  NVIC_EnableIRQ(BUT_PIO_ID);
-  NVIC_SetPriority(BUT_PIO_ID, 4);
+void TC_init(Tc *TC, int ID_TC, int TC_CHANNEL, int freq) {
+  uint32_t ul_div;
+  uint32_t ul_tcclks;
+  uint32_t ul_sysclk = sysclk_get_cpu_hz();
 
-  /* conf botão como entrada */
-  pio_configure(BUT_PIO, PIO_INPUT, BUT_PIO_PIN_MASK,
-                PIO_PULLUP | PIO_DEBOUNCE);
-  pio_set_debounce_filter(BUT_PIO, BUT_PIO_PIN_MASK, 60);
-  pio_enable_interrupt(BUT_PIO, BUT_PIO_PIN_MASK);
-  pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIO_PIN_MASK, PIO_IT_FALL_EDGE,
-                  but_callback);
+  pmc_enable_periph_clk(ID_TC);
+
+  tc_find_mck_divisor(freq, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
+  tc_init(TC, TC_CHANNEL, ul_tcclks | TC_CMR_CPCTRG);
+  tc_write_rc(TC, TC_CHANNEL, (ul_sysclk / ul_div) / freq);
+
+  NVIC_SetPriority((IRQn_Type)ID_TC, 4);
+  NVIC_EnableIRQ((IRQn_Type)ID_TC);
+  tc_enable_interrupt(TC, TC_CHANNEL, TC_IER_CPCS);
+
+  tc_start(TC, TC_CHANNEL);
 }
 
 static void USART1_init(void) {
@@ -253,27 +258,14 @@ int main(void) {
   USART1_init();
   configure_console();
 
-  /* Attempt to create a semaphore. */
-  xSemaphoreBut = xSemaphoreCreateBinary();
-  if (xSemaphoreBut == NULL)
+  xQueueADC = xQueueCreate(100, sizeof(adcData));
+  if (xSemaphore == NULL)
     printf("falha em criar o semaforo \n");
 
-  /* cria queue com 32 "espacos" */
-  /* cada espaço possui o tamanho de um inteiro*/
-  xQueueLedFreq = xQueueCreate(32, sizeof(uint32_t));
-  if (xQueueLedFreq == NULL)
-    printf("falha em criar a queue \n");
-
   /* Create task to make led blink */
-  if (xTaskCreate(task_led, "Led", TASK_LED_STACK_SIZE, NULL,
-                  TASK_LED_STACK_PRIORITY, NULL) != pdPASS) {
-    printf("Failed to create test led task\r\n");
-  }
-
-  /* Create task to monitor processor activity */
-  if (xTaskCreate(task_but, "BUT", TASK_BUT_STACK_SIZE, NULL,
-                  TASK_BUT_STACK_PRIORITY, NULL) != pdPASS) {
-    printf("Failed to create UartTx task\r\n");
+  if (xTaskCreate(task_adc, "ADC", TASK_ADC_STACK_SIZE, NULL,
+                  TASK_ADC_STACK_PRIORITY, NULL) != pdPASS) {
+    printf("Failed to create test ADC task\r\n");
   }
 
   /* Start the scheduler. */
