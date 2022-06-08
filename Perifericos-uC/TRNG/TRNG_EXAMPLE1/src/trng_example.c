694a87fe697fa14125a979fb95bac8c37357a59a
Bruno Boldrim Saboya <62663074+BrunoSaboya@users.noreply.github.com>
2022-06-08 16:49:31 -0300

    Exemplo Lib C Rand()

--- Perifericos-uC/TRNG/TRNG_EXAMPLE1/src/trng_example.c
+++ Perifericos-uC/TRNG/TRNG_EXAMPLE1/src/trng_example.c
@@ -1,13 +1,6 @@
 #include "asf.h"
-
-#define BUT_PIO			  PIOA
-#define BUT_PIO_ID		  ID_PIOA
-#define BUT_PIO_IDX		  11
-#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
-
-volatile int but_flag = 0;
-
-void init(void);
+#include <stdio.h>
+#include <stdlib.h>
 
 static void configure_console(void)
 {
@@ -25,10 +18,6 @@ static void configure_console(void)
 	stdio_serial_init(CONF_UART, &uart_serial_options);
 }
 
-void but_callback(void){
-	but_flag = 1;
-}
-
 /*
 Esse é o handler do TRNG que quando for chamado
 realiza a criação de mais um número aleatório
@@ -36,29 +25,19 @@ realiza a criação de mais um número aleatório
 void TRNG_Handler(void)
 {
 
-		// Variável para sabermos se foi ou não acionado o handler
-		uint32_t status;
+	// Variável para sabermos se foi ou não acionado o handler
+	uint32_t status;
 
-		// Guardamos o valor desse status com essa função de interrupção
-		status = trng_get_interrupt_status(TRNG);
+	// Guardamos o valor desse status com essa função de interrupção
+	status = trng_get_interrupt_status(TRNG);
 	
-		// Verificamos se foi ou nao acionado a chamada da função, e caso tenha sido chamada
-		// Escrevemos no terminal a saída do output do TRNG que foi escolhido previamente
-		if ((status & TRNG_ISR_DATRDY) == TRNG_ISR_DATRDY) {
-			printf("-- Random Value: %lx --\n\r", trng_read_output_data(TRNG));
-		}
-}
+	// Verificamos se foi ou nao acionado a chamada da função, e caso tenha sido chamada
+	// Escrevemos no terminal a saída do output do TRNG que foi escolhido previamente
+	if ((status & TRNG_ISR_DATRDY) == TRNG_ISR_DATRDY) {
+		//printf("-- Random Value: %lx --\n\r", trng_read_output_data(TRNG));
+		//printf("%lx\n\r", trng_read_output_data(TRNG));
 
-void init(void){
-	pmc_enable_periph_clk(BUT_PIO_ID);
-	pio_configure(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK,
-	PIO_PULLUP | PIO_DEBOUNCE);
-	pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIO_IDX_MASK, PIO_IT_EDGE,
-	but_callback);
-	pio_enable_interrupt(BUT_PIO, BUT_PIO_IDX_MASK);
-	pio_get_interrupt_status(BUT_PIO);
-	NVIC_EnableIRQ(BUT_PIO_ID);
-	NVIC_SetPriority(BUT_PIO_ID, 4);
+	}
 }
 
 //parte principal do código que é rodado ao iniciar a placa
@@ -66,8 +45,7 @@ int main(void)
 {
 	sysclk_init();
 	board_init();
-	init();
-
+	
 	configure_console();
 	
 	printf("-- TRNG Example --\n\r");
@@ -84,7 +62,11 @@ int main(void)
 	NVIC_EnableIRQ(TRNG_IRQn);
 	trng_enable_interrupt(TRNG);
 
+	int i, n;
+	time_t t;
+	n = 5;
+	
 	while (1) {
-		
+		printf("%d\n", rand());
 	}
 }
