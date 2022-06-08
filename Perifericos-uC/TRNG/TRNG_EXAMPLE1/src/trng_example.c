5e55cf07b1f5849f4ca70c44d158c81dae9731bf
Bruno Boldrim Saboya <62663074+BrunoSaboya@users.noreply.github.com>
2022-06-08 15:32:47 -0300

    Comentando o código

--- Perifericos-uC/TRNG/TRNG_EXAMPLE1/src/trng_example.c
+++ Perifericos-uC/TRNG/TRNG_EXAMPLE1/src/trng_example.c
@@ -1,81 +1,14 @@
-/**
- * \file
- *
- * \brief True Random Number Generator (TRNG) example for SAM.
- *
- * Copyright (c) 2011-2018 Microchip Technology Inc. and its subsidiaries.
- *
- * \asf_license_start
- *
- * \page License
- *
- * Subject to your compliance with these terms, you may use Microchip
- * software and any derivatives exclusively with Microchip products.
- * It is your responsibility to comply with third party license terms applicable
- * to your use of third party software (including open source software) that
- * may accompany Microchip software.
- *
- * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES,
- * WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
- * INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
- * AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
- * LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
- * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
- * SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
- * POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
- * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
- * RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
- * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
- *
- * \asf_license_stop
- *
- */
-
-/**
- * \mainpage TRNG Example
- *
- * \section Purpose
- *
- * This example demonstrates the True Random Number Generator (TRNG) provided on
- * SAM microcontroller. It enables the user to enable TRNG to generate random values
- * every 84 Clock Cycles.
- *
- * \section Requirements
- *
- * This package can be used with SAM evaluation kits.
- *
- * \section Description
- *
- * When launched, this program displays generated random values on the terminal.
- *
- * \section Usage
- *
- * -# Build the program and download it into the evaluation board.
- * -# On the computer, open and configure a terminal application
- *    (e.g., HyperTerminal on Microsoft Windows) with these settings:
- *   - 115200 bauds
- *   - 8 bits of data
- *   - No parity
- *   - 1 stop bit
- *   - No flow control
- * -# Start the application.
- * -# In the terminal window, the following text should appear:
- *    \code
-	-- TRNG Example --
-	-- xxxxxx-xx
-	-- Compiled: xxx xx xxxx xx:xx:xx --
-	Random Value: xxxx
-\endcode
- */
-/*
- * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
- */
-
 #include "asf.h"
 
-/**
- * \brief Configure the console UART.
- */
+#define BUT_PIO			  PIOA
+#define BUT_PIO_ID		  ID_PIOA
+#define BUT_PIO_IDX		  11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
+
+volatile int but_flag = 0;
+
+void init(void);
+
 static void configure_console(void)
 {
 	const usart_serial_options_t uart_serial_options = {
@@ -88,63 +21,70 @@ static void configure_console(void)
 		.stopbits = CONF_UART_STOP_BITS,
 #endif
 	};
-	
-	/* Configure console UART. */
 	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
 	stdio_serial_init(CONF_UART, &uart_serial_options);
 }
 
-/**
- * \brief Interrupt handler for the TRNG.
- *
- * Display the random value on the terminal.
- */
-void TRNG_Handler(void)
-{
-	uint32_t status;
-
-	status = trng_get_interrupt_status(TRNG);
-
-	if ((status & TRNG_ISR_DATRDY) == TRNG_ISR_DATRDY) {
-		printf("-- Random Value: %lx --\n\r", trng_read_output_data(TRNG));
-	}
+void but_callback(void){
+	but_flag = 1;
 }
 
-/**
- * \brief Application entry point for TRNG example.
- *
- * Enable the TRNG, display the generated random value.
- *
- * \return Unused (ANSI-C compatibility).
- */
+/*
+Esse é o handler do TRNG que quando for chamado
+realiza a criação de mais um número aleatório
+*/
+void TRNG_Handler(void)
+{
+
+		// Variável para sabermos se foi ou não acionado o handler
+		uint32_t status;
+
+		// Guardamos o valor desse status com essa função de interrupção
+		status = trng_get_interrupt_status(TRNG);
+	
+		// Verificamos se foi ou nao acionado a chamada da função, e caso tenha sido chamada
+		// Escrevemos no terminal a saída do output do TRNG que foi escolhido previamente
+		if ((status & TRNG_ISR_DATRDY) == TRNG_ISR_DATRDY) {
+			printf("-- Random Value: %lx --\n\r", trng_read_output_data(TRNG));
+		}
+}
+
+void init(void){
+	pmc_enable_periph_clk(BUT_PIO_ID);
+	pio_configure(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK,
+	PIO_PULLUP | PIO_DEBOUNCE);
+	pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIO_IDX_MASK, PIO_IT_EDGE,
+	but_callback);
+	pio_enable_interrupt(BUT_PIO, BUT_PIO_IDX_MASK);
+	pio_get_interrupt_status(BUT_PIO);
+	NVIC_EnableIRQ(BUT_PIO_ID);
+	NVIC_SetPriority(BUT_PIO_ID, 4);
+}
+
+//parte principal do código que é rodado ao iniciar a placa
 int main(void)
 {
-	/* Initialize the SAM system */
 	sysclk_init();
 	board_init();
+	init();
 
-	/* Configure console UART */
 	configure_console();
-
-	/* Output example information */
+	
 	printf("-- TRNG Example --\n\r");
 	printf("-- %s\n\r", BOARD_NAME);
-	printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
+	printf("-- Compiled: %s %s --\n\r\n", __DATE__, __TIME__);
 
-	/* Configure PMC */
 	pmc_enable_periph_clk(ID_TRNG);
 
-	/* Enable TRNG */
 	trng_enable(TRNG);
 
-	/* Enable TRNG interrupt */
 	NVIC_DisableIRQ(TRNG_IRQn);
 	NVIC_ClearPendingIRQ(TRNG_IRQn);
 	NVIC_SetPriority(TRNG_IRQn, 0);
 	NVIC_EnableIRQ(TRNG_IRQn);
 	trng_enable_interrupt(TRNG);
 
-	/* User input loop */
 	while (1) {
+		
 	}
 }
